91297054428eb86bbe54c32af83aa6f7b08474e6
Elrey Belmonti and Linda Luu <linda@>
2017-08-17 09:29:43 -0700

    add logic for ones place numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,32 @@
+var converterTable = {
+  1: "I",
+  5: "V",
+  10: "X",
+  50: "L",
+  100: "C",
+  500: "D",
+  1000: "M"
+};
+
+var roman = "";
+var numbers =Object.keys(converterTable);
+
+// numbers.forEach(function(number){
+//   roman = converterTable[number];
+//   console.log(roman);
+// });
+
+
+
 $(document).ready(function(){
-  $("form#number").submit(function(event){
+  $("form#number-form").submit(function(event){
     event.preventDefault();
+    var numberInput = parseInt($("input#number").val());
+
+    if (numberInput % 1 === 0){
+      roman = "I".repeat(numberInput);
+    }
+
+    $(".roman").text(roman);
   });
 });
