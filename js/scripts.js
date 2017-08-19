83b2e3c490b2291805e83b17a84b056092495fa5
Elrey Belmonti <elbelmonti@gmail.com>
2017-08-18 20:30:05 -0700

    add logic to converter arabic numbers to roman numberals

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,47 @@
-// var converterTable = {
-//   1: "I",
-//   5: "V",
-//   10: "X",
-//   50: "L",
-//   100: "C",
-//   500: "D",
-//   1000: "M"
-// };
-//
-// var remainder = 0;
-// var roman = "";
-// var numbers =Object.keys(converterTable);
-//
-// // numbers.forEach(function(number){
-// //   roman = converterTable[number];
-// //   console.log(roman);
-// // });
+// hash to hold all the possible characters for roman numberals(keys) and arabic numbers(values)
+var romanNumberalsTable = {
+   M: 1000,
+  CM: 900,
+   D: 500,
+  CD: 400,
+   C: 100,
+  XC: 90,
+   L: 50,
+  XL: 40,
+   X: 10,
+  IX: 9,
+   V: 5,
+  IV: 4,
+   I: 1
+};
 
+// an array to hold the keys(roman numberals) of roman numberal table
+var romanNumberals = Object.keys(romanNumberalsTable);
 
+// empty string to hold the roman numberal that will be built base off the user input
+var result = "";
+
+// function to to converter arabic numbers to roman numberals
+function romanNumberalsConverter(arabicNumber){
+  // For each roman numberal in the roman numberals array
+  romanNumberals.forEach(function(romanNumberal){
+
+    // while arabic number is greater than the value at the key in the hash
+    // Ex. arabicNumber = 349  >= romanNumberal
+    while(arabicNumber >= romanNumberalsTable[romanNumberal]){
+      result += romanNumberal;
+      arabicNumber -= romanNumberalsTable[romanNumberal];
+    }
+  });
+  return result;
+}
 
 $(document).ready(function(){
   $("form#number-form").submit(function(event){
     event.preventDefault();
+
     var numberInput = parseInt($("input#number").val());
 
-
-
-    if (numberInput > 10 || numberInput % 10 === 0){
-      var difference = numberInput % 10;
-      console.log(difference);
-      var tenthPlace = numberInput % 10;
-      roman = "X".repeat(tenthPlace);
-      roman += "I".repeat(difference);
-    } else if (numberInput === 19){
-      roman = "XIX";
-    } else if (numberInput > 15){
-      remainder = numberInput - 15;
-      roman = "XV" + "I".repeat(remainder);
-    } else if (numberInput === 15){
-      roman = "XV";
-    } else if (numberInput === 14){
-      roman = "XIV";
-    } else if (numberInput > 10){
-      remainder = numberInput - 10;
-      roman = "X" + "I".repeat(remainder);
-    } else if (numberInput % 10 === 0){
-      roman = "X";
-    } else if (numberInput === 9){
-      roman = "IX";
-    } else if (numberInput > 5){
-      remainder = numberInput - 5;
-      roman = "V" + "I".repeat(remainder);
-    } else if (numberInput === 5) {
-      roman = "V";
-    } else if (numberInput === 4){
-      roman = "IV";
-    } else if (numberInput % 1 === 0){
-      roman = "I".repeat(numberInput);
-    }
-
-    $(".roman").text(roman);
+    $(".roman").text(romanNumberalsConverter(numberInput));
   });
 });
