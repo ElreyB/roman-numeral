18c639f0111ff474075a19ef054d77e0956569db
Elrey Belmonti and Mitchell Long <mitchell@>
2017-10-10 09:41:42 -0700

    concatenated js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
 
-gulp.task('jsBrowserify', function() {
-  return browserify({ entries: ['./js/roman-numeral-interface.js'] })
+
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
+
+
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
