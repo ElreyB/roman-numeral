e63f8f854f3066af5dc8e9ceaaf110e4e4cf2595
Elrey Belmonti and Mitchell Long <elrey@>
2017-10-10 09:59:10 -0700

    WIP fixing syntax error

--- build/js/app.js
+++ build/js/app.js
@@ -1,55 +1,4 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var Converter = require('./../js/roman-numeral.js').converterModule;
-
-$(document).ready(function(){
-  $("span.roman, span.arabic").hide();
-
-  var converter = new Converter();
-
-  $("form#number-form").submit(function(event){
-    event.preventDefault();
-    $(this).find(".form-group").removeClass("has-error");
-    $("p.number-error").hide();
-
-    var numberInput = parseInt($("input#number").val());
-
-    if (isNaN(numberInput)){
-      $(this).find(".form-group").addClass("has-error");
-      $("p.number-error").show();
-      return;
-    }
-
-    $("span.roman").text(converter.romanNumeralsConverter(numberInput)).show();
-    $("input#number").val("");
-  });
-
-  $("#clear-a").click(function(event){
-    event.preventDefault();
-    $("span.roman").text("").hide();
-  })
-
-  $("form#arabic-form").submit(function(event){
-    event.preventDefault();
-    $(this).find(".form-group").removeClass("has-error");
-    $("p.roman-error").hide();
-    var romanInput = $("input#roman").val().toUpperCase();
-
-    if (romanInput.match(/[^MDCLXVI]/)){
-      $(this).find(".form-group").addClass("has-error");
-      $("p.roman-error").show();
-      return;
-    }
-    $("span.arabic").text(converter.arabicNumberConverter(romanInput)).show();
-    $("input#roman").val("");
-  });
-
-  $("#clear-r").click(function(event){
-    event.preventDefault();
-    $("span.arabic").text("").hide();
-  })
-});
-
-},{"./../js/roman-numeral.js":2}],2:[function(require,module,exports){
 // hash to hold all the possible characters for roman numberals(keys) and arabic numbers(values)
 var romanNumeralsTable = {
    M: 1000,
@@ -110,4 +59,55 @@ Converter.prototype.arabicNumberConverter = function (romanNumeral = ""){
 
 exports.converterModule = Converter;
 
-},{}]},{},[1]);
+},{}],2:[function(require,module,exports){
+var Converter = require('./../js/roman-numeral.js').converterModule;
+
+$(document).ready(function(){
+  $("span.roman, span.arabic").hide();
+
+  var converter = new Converter();
+
+  $("form#number-form").submit(function(event){
+    event.preventDefault();
+    $(this).find(".form-group").removeClass("has-error");
+    $("p.number-error").hide();
+
+    var numberInput = parseInt($("input#number").val());
+
+    if (isNaN(numberInput)){
+      $(this).find(".form-group").addClass("has-error");
+      $("p.number-error").show();
+      return;
+    }
+
+    $("span.roman").text(converter.romanNumeralsConverter(numberInput)).show();
+    $("input#number").val("");
+  });
+
+  $("#clear-a").click(function(event){
+    event.preventDefault();
+    $("span.roman").text("").hide();
+  })
+
+  $("form#arabic-form").submit(function(event){
+    event.preventDefault();
+    $(this).find(".form-group").removeClass("has-error");
+    $("p.roman-error").hide();
+    var romanInput = $("input#roman").val().toUpperCase();
+
+    if (romanInput.match(/[^MDCLXVI]/)){
+      $(this).find(".form-group").addClass("has-error");
+      $("p.roman-error").show();
+      return;
+    }
+    $("span.arabic").text(converter.arabicNumberConverter(romanInput)).show();
+    $("input#roman").val("");
+  });
+
+  $("#clear-r").click(function(event){
+    event.preventDefault();
+    $("span.arabic").text("").hide();
+  })
+});
+
+},{"./../js/roman-numeral.js":1}]},{},[2]);
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,13 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
+function createErrorHandler(name) {
+  return function (err) {
+    console.error('Error from ' + name + ' in compress task', err.toString());
+  };
+}
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
@@ -16,4 +22,13 @@ gulp.task('jsBrowserify', ['concatInterface'], function() {
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task("minifyScripts", ["jsBrowserify"], function(){
+  return gulp.src("./build/js/app.js")
+  .on('error', createErrorHandler('gulp.src'))
+    .pipe(uglify())
+     .on('error', createErrorHandler('uglify'))
+    .pipe(gulp.dest("./build/js"))
+    .on('error', createErrorHandler('gulp.dest'));
 });
